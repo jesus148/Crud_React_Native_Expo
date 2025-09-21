9d21ad867ad4b3ad12cfc5ab14966dd85674e800
Jesus Argel <90942888+jesus148@users.noreply.github.com>
2025-09-20 19:36:47 -0500

    test

--- core/auth/api/productsApi.ts
+++ core/auth/api/productsApi.ts
@@ -1,3 +1,4 @@
+import { SecureStorageAdapter } from "@/helpers/adapters/secury-storage.adapter";
 import axios from "axios";
 import { Platform } from "react-native";
 // instancia de axios
@@ -15,10 +16,10 @@ export const API_URL =
     : process.env.EXPO_PUBLIC_API_URL_ANDROID; //si es android , poner tu ip
 
 //  LOG  {"android": "http://192.168.100.5:3000/api", "stage": "dev"}
-console.log({
-  stage: STAGE,
-  [Platform.OS]: API_URL,
-});
+// console.log({
+//   stage: STAGE,
+//   [Platform.OS]: API_URL,
+// });
 
 const productsApi = axios.create({
   // baseURL:'localhost:3000/api'
@@ -26,6 +27,22 @@ const productsApi = axios.create({
   // base url de tu pc para el backend
   baseURL: API_URL,
 });
+
+
+// intercetor osea cade vez q en toda tu app hagas una request esto se envia
+productsApi.interceptors.request.use(async(config)=>{
+
+  // obtiene el token
+  const token = await SecureStorageAdapter.getitem('token');
+
+  // existe token
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  // retorna
+  return config
+})
+
 
 export { productsApi };
 
--- helpers/adapters/secury-storage.adapter.ts
+++ helpers/adapters/secury-storage.adapter.ts
@@ -1,17 +1,39 @@
 
-
+import * as SecureStore from 'expo-secure-store';
+import { Alert } from 'react-native';
 export class SecureStorageAdapter{
 
+    // CLASE PARA GUARDAR O ALMACENAR INFORMACION SENSIBLE COMO TOKEN.ETC
+
+
+
+    // metodo para setear o guardar el token mediante una key
     static async setItem(key:string, value:string){
-
+        try {
+            await SecureStore.setItemAsync(key, value);
+        } catch (error) {
+            Alert.alert('Error', 'Failed to save data')
+        }
     }
 
+    // obteniendo el token x una key
     static async getitem(key:string){
-
+        try {
+            return await SecureStore.getItemAsync(key);
+        } catch (error) {
+            Alert.alert('Error', 'Failed to get data');
+            return null;
+        }
     }
 
+    // eliminando el token
     static async deleteitem(key:string){
-        
+        try {
+            await SecureStore.deleteItemAsync(key);
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Error', 'Failed to delete data')
+        }
     }
 
 }
